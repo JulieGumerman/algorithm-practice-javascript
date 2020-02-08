33038b9723d9b973e8e280d331cf85ad52d06b96
bodhibrdr <julie.gumerman@gmail.com>
2020-02-08 08:21:22 -0700

    yay

---
+++ count-up-and-down.js
@@ -0,0 +1,12 @@
+const countUpAndDown = (num) => {
+    for (let i = 0; i <= num; i++) {
+        console.log(i)
+    }
+    console.log("yeet")
+    for (let i = num; i >= 0; i--) {
+        console.log(i)
+    }
+    console.log("bob")
+}
+
+countUpAndDown(5)
