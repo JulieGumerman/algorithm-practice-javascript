4675e217ce014a90e706bc593fb8e937bf77a327
bodhibrdr <julie.gumerman@gmail.com>
2020-02-21 20:41:39 -0700

    getting the right subarrays now :-)

--- max-sub-array-sum.js
+++ max-sub-array-sum.js
@@ -5,9 +5,26 @@
 //4. must consist of consecutive elements in array
 
 //The plan:
-//1. Suck my thumb and cry
+//1. Suck my thumb and cry ✔ 
 //For loop?
 //cycle through and make an array of however many n numbers
 //For loop # 2 (keeping it O(n) by keeping the loops separate and not nested)
 //tally totals 
 //return larges
+
+function maxSubarraySum(array, number) {
+    let subarrays = []
+    let counter = number - 1
+    for (let i = 0; i < array.length; i++) {
+        console.log("counter!!!", counter)
+        let endpoint = counter + i
+        console.log("endpoint!!!", endpoint)
+        let newArr = array.slice(i, number + i)
+        subarrays.push(newArr)
+    }
+    console.log(subarrays)
+
+
+}
+
+maxSubarraySum([100, 200, 300, 400], 2)
