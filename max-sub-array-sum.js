967fe9a2c3d74b5b6d189b853f7ca0173effcbea
bodhibrdr <julie.gumerman@gmail.com>
2020-02-21 20:27:21 -0700

    step 1: make a plan

---
+++ max-sub-array-sum.js
@@ -0,0 +1,13 @@
+//In my own words:
+//1. The second parameter is a number n. 
+//2. Add together the largest n numbers to return the largest sum
+//3. Something consecutive something blablabla
+//4. must consist of consecutive elements in array
+
+//The plan:
+//1. Suck my thumb and cry
+//For loop?
+//cycle through and make an array of however many n numbers
+//For loop # 2 (keeping it O(n) by keeping the loops separate and not nested)
+//tally totals 
+//return larges
