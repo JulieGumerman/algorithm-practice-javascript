5c897882534fa5e6f17d461964e395e748b52729
bodhibrdr <julie.gumerman@gmail.com>
2020-03-08 19:48:31 -0600

    bst insert

--- binary-search-tree.js
+++ binary-search-tree.js
@@ -10,6 +10,34 @@ class BinarySearchTree {
     constructor() {
         this.root = null
     }
+
+    insert(value) {
+        let newNode = new Node(value)
+        if (value === current.value) return undefined
+        if(this.root === null) {
+            this.root = newNode
+            return this
+        } else {
+            let current = this.root
+            while(true) {
+                if (value < current.value) {
+                    if (current.left === null) {
+                        current.left = newNode
+                        return this
+                    } else {
+                        current = current.left;
+                    }
+                } else if (value > current.value) {
+                    if (current.right === null) {
+                        current.right = newNode
+                        return this
+                    } else {
+                        current = current.right
+                    }
+                }
+            }
+        }
+    }
 }
 
 let tree = new BinarySearchTree();
