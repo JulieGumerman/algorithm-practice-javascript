d99bf23adf4ac06e56a04d03c5399bdbd4f37929
bodhibrdr <julie.gumerman@gmail.com>
2020-03-11 09:59:54 -0600

    add an edgegit add .git add .

--- graphs.js
+++ graphs.js
@@ -4,11 +4,17 @@ class Graph{
     }
 
     addVertex (vertex) {
-        this.adjacencyList[vertex] = []
+        if(!this.adjacencyList[vertex]) this.adjacencyList[vertex] = []
     }
-}
+
+    addEdge(vertex1, vertex2) {
+        this.adjacencyList[vertex1].push(vertex2)
+        this.adjacencyList[vertex2].push(vertex1)
+    }
+} 
 
 g = new Graph()
-g.addVertex("Mt. Rainier")
-g.addVertex("Mt. Baker")
+g.addVertex("Denver")
+g.addVertex("Seattle")
+g.addEdge("Denver", "Seattle")
 console.log(g)
