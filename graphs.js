b13e33150dc1af7a6402afdd11b21888bea15cf8
bodhibrdr <julie.gumerman@gmail.com>
2020-03-11 18:32:59 -0600

    remove edge works

--- graphs.js
+++ graphs.js
@@ -11,10 +11,21 @@ class Graph{
         this.adjacencyList[vertex1].push(vertex2)
         this.adjacencyList[vertex2].push(vertex1)
     }
+
+    removeEdge(v1, v2) {
+        this.adjacencyList[v1] = this.adjacencyList[v1].filter(item => {
+            return item !== v2
+        })
+        this.adjacencyList[v2] = this.adjacencyList[v2].filter(item => {
+            return item != v1
+        })
+    }
 } 
 
 g = new Graph()
 g.addVertex("Denver")
 g.addVertex("Seattle")
 g.addEdge("Denver", "Seattle")
+console.log(g)
+g.removeEdge("Denver", "Seattle")
 console.log(g)
