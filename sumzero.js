64d3736e701d141f7a6e7ee79516061da21c4e71
bodhibrdr <julie.gumerman@gmail.com>
2020-04-02 14:54:45 -0600

    sumzero solution 1

---
+++ sumzero.js
@@ -0,0 +1,21 @@
+const sumZero = sortedArray => {
+    let perfectPair = [];
+    for (let i = 0; i < sortedArray.length; i++) {
+        for (let j = 0; j < sortedArray.length; j++) {
+            if (sortedArray[i] + sortedArray[j] === 0 && sortedArray[i] !== sortedArray[j]) {
+                perfectPair.push(sortedArray[i])
+                perfectPair.push(sortedArray[j])
+                return perfectPair
+            }
+        }
+    }
+    // if (perfectPair.length === 0) {
+    //     console.log(undefined)
+    // } else {
+    //     console.log(perfectPair)
+    // }
+    return undefined
+}
+
+console.log(sumZero([-2, 0, 1, 3]))
+console.log(sumZero([-3, -2, 0, 3]))
