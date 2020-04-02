b26614a80cee6218eee94a50f6ff3b8009d33816
bodhibrdr <julie.gumerman@gmail.com>
2020-04-02 16:26:01 -0600

    sum-zero function with pointers

--- sumzero.js
+++ sumzero.js
@@ -12,5 +12,19 @@ const sumZero = sortedArray => {
     return undefined
 }
 
-console.log(sumZero([-2, 0, 1, 3]))
-console.log(sumZero([-3, -2, 0, 3]))
+const sumZeroPointers = sortedArray => {
+    let left = 0;
+    let right = sortedArray.length - 1;
+    while (left < right) {
+        let sum = sortedArray[left] + sortedArray[right];
+        if (sum === 0) {
+            return[sortedArray[left], sortedArray[right]];
+        } else if (sum > 0) {
+            right--;
+        } else {
+            left++
+        }
+    }
+}
+console.log(sumZeroPointers([-2, 0, 1, 3]))
+console.log(sumZeroPointers([-3, -2, 0, 3]))
