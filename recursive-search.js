3f46ec813a0607f54100994b6832c7cbdfeaac5a
bodhibrdr <julie.gumerman@gmail.com>
2020-04-03 16:13:27 -0600

    recursive-search plan

---
+++ recursive-search.js
@@ -0,0 +1,8 @@
+const rs = (arr, num) => {
+    //if arr == num
+        //return index
+    //if arr !== num
+        //return rs of next num in array, num
+    //if last item !== num
+        //return -1
+}
