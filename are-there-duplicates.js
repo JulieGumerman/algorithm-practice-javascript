93c524433e3f08f2e715c5fd7d4cb8762e89b836
bodhibrdr <julie.gumerman@gmail.com>
2020-04-06 15:51:21 -0600

    are-there-duplicates works

--- are-there-duplicates.js
+++ are-there-duplicates.js
@@ -9,9 +9,19 @@ const areThereDuplicates = (...args) => {
         //if you find a whatnot in the dictionary, 
             //return true
         //add each whatnot to the dictionary to catalog it
-    
+    for (let i = 0; i < args.length; i++) {
+        if (catalog[args[i]] === "Wheee") {
+            return console.log(true)
+        }
+        catalog[args[i]] = "Wheee"
+        console.log("CATALOG", catalog)
+    }
+
     //Step 3 (optional):
     //if "found in dictionary" return statement is never hit, return true
+    return console.log(false)
 }
 
 areThereDuplicates(1, 2, 3, 4, 5)
+
+areThereDuplicates('a', 'b', 'c', 'a')
