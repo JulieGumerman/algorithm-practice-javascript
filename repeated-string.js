6e302d8b118cf5c8b2d4b3346a710e64e81c1736
bodhibrdr <julie.gumerman@gmail.com>
2020-04-07 13:04:21 -0600

    heck yeah it works

--- repeated-string.js
+++ repeated-string.js
@@ -4,20 +4,35 @@ const repeatedString = (s, n) => {
 
 
     let remainder = n % s.length
+    let divided = Math.floor(n / s.length)
+    console.log("DIVIDED", divided)
 
-    let infiniteArray = s.split("")
-    let finiteArray = infiniteArray + "," + infiniteArray.slice(0, remainder)
-    console.log(finiteArray)
+
+    
+
+    let array = s.split("")
+    let tinyArray = array.slice(0, remainder)
+
 
     let counter = 0;
+    let microCounter = 0;
 
-    for (let i = 0; i < finiteArray.length; i++) {
-        if (finiteArray[i] === "a") {
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === "a") {
             counter += 1
-            console.log(counter, infiniteArray[i])
+            console.log(counter, array[i])
         }
     }
-    return counter
+    console.log("TINYARRAY", tinyArray)
+    for (let i = 0; i < tinyArray.length; i++) {
+        if (tinyArray[i] === "a") {
+            microCounter += 1
+            console.log("FROM LIL BIT", microCounter, tinyArray[i])
+        }
+    }
+
+    let totalAs = (counter * divided) + microCounter
+    return console.log(totalAs)
 
 }
 
