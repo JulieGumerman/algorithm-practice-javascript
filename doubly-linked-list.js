8c04e58445c0865906032a02662b25f4dd311d42
bodhibrdr <julie.gumerman@gmail.com>
2020-04-08 12:51:55 -0600

    push functionality for DLL

--- doubly-linked-list.js
+++ doubly-linked-list.js
@@ -27,4 +27,53 @@ class DoublyLinkedList {
         this.tail = null;
         this.length = 0;
     }
+
+    push(val) {
+        new_node = new Node(val)
+        if (this.head === null) {
+            this.head = new_node
+            this.tail = new_node
+        } else {
+            this.tail.next = new_node;
+            this.tail.nex.prev = this.tail
+            this.tail = this.tail.next
+        }
+
+        
+        
+        
+        
+        
+    }
+
+    pop() {
+        return
+    }
+
+    shift() {
+        return
+    }
+
+    unshift() {
+        return
+    }
+
+    get() {
+        return
+    }
+    set() {
+        return
+    }
+    insert() {
+        return
+    }
+    remove() {
+        return
+    }
 }
+
+first = new Node (12)
+first.next = new Node(13);
+first.next.prev = first
+
+console.log(first)
