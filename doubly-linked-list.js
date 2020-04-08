c4bfe77f8c9aca0b988bc1db34c77d07d8227e22
bodhibrdr <julie.gumerman@gmail.com>
2020-04-08 13:50:47 -0600

    shift functionality

--- doubly-linked-list.js
+++ doubly-linked-list.js
@@ -60,7 +60,23 @@ class DoublyLinkedList {
     }
 
     shift() {
-        return
+        if (this.head === null) {
+            return undefined
+        } 
+        
+        let oldHead = this.head;
+
+        if (this.length === 1) {
+            this.head = null
+            this.tail = null
+        } else {
+            this.head = this.head.next
+            this.head.prev = null
+            oldHead.next = null
+        }
+
+        this.length--
+        return oldHead
     }
 
     unshift() {
