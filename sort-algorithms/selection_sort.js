8ea4b4beaad136e92876c58cead72254fab2dcbb
bodhibrdr <julie.gumerman@gmail.com>
2020-06-12 09:13:23 -0600

    good ol' selection sort

---
+++ sort-algorithms/selection_sort.js
@@ -0,0 +1,14 @@
+function selectionSort (arr) {
+
+    for (var i = 0; i < arr.length; i++) {
+        var lowest = i;
+
+        for (var j = i + 1; j < arr.length; j++) {
+            console.log(i, j)
+        }
+
+    }
+    return arr;
+}
+
+selectionSort([22, 34, 10, 19, 17])
